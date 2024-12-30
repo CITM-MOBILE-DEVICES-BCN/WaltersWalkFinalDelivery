855ee6530773be5c5c938f90fb4241ea05173a19
VM Marco <59030832+Marco-v-BaldanII@users.noreply.github.com>
2024-12-30 20:33:06 +0100

    experimentation

---
+++ WaltersWalk/Assets/WW_Scripts/MapGeneration/WW_MapCreator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace WalterWalk
+{
+    public class WW_MapCreator : MonoBehaviour
+	{
+    	
+
+    	
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
