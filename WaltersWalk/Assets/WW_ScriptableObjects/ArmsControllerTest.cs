3ec1d9d8225301efa7d56f55af8c3c44a32ff579
sgantzer12 <74564975+sgantzer12@users.noreply.github.com>
2025-01-16 11:43:26 +0100

    test scene for arms animations

---
+++ WaltersWalk/Assets/WW_ScriptableObjects/ArmsControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmsControllerTest : MonoBehaviour
+{
+	private Animator animator;
+	private bool isPhoneActive = false;
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			animator.SetBool("isPhoneActive", true);
+			animator.SetTrigger("PhoneOut");
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("isPhoneActive", false);
+            animator.SetTrigger("StorePhone");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+		{
+            animator.SetTrigger("Scratching");
+		}
+
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+            animator.SetTrigger("Pills");
+		}
+
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+            animator.SetTrigger("SmokeCig");
+		}
+	}
+}
