e865351b994068e4a2ac07eb68bee5f6cb3c2318
Peluchomp <pau.mora.sole@gmail.com>
2025-01-19 17:21:53 +0100

    Moved Folders and updated prefab

---
+++ WaltersWalk/Assets/WaltersWalk/WW_ScriptableObjects/ArmsControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmsControllerTest : MonoBehaviour
+{
+    private Animator animator;
+    private AudioSource audioSource;
+    public AudioClip pillsClip;
+    public AudioClip smokingClip;
+    public AudioClip scratchingClip;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.SetBool("IsPhoneActive", true);
+            animator.SetTrigger("PhoneOut");
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("IsPhoneActive", false);
+            animator.SetTrigger("StorePhone");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetTrigger("Scratching");
+            audioSource.clip = scratchingClip;
+            audioSource.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            animator.SetTrigger("Pills");
+            audioSource.clip = pillsClip;
+            audioSource.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            animator.SetTrigger("SmokeCig");
+            audioSource.clip = smokingClip;
+            audioSource.Play();
+        }
+    }
+}
+
