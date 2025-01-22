3fa71efe7154a76c3ded63f844042c50c792225e
Onnda <dannoman2004@gmail.com>
2025-01-22 11:51:38 +0100

    CameraMovement+ScriptANimation

--- WaltersWalk/Assets/WaltersWalk/WW_Art/Modelos Dani/gangster/ShadyScript.cs
+++ WaltersWalk/Assets/WaltersWalk/WW_Art/Modelos Dani/gangster/ShadyScript.cs
@@ -6,8 +6,12 @@ public class ShadyScript : MonoBehaviour
 {
 	public Animator Shady;
 	
+	public Animator Camera;
+	
 	public void OpenShop()
 	{
 		Shady.SetBool("OpenShop", true);
+		
+		Camera.SetBool("ShadySHop",true);
 	}
 }
