b73f33677fce1f6d06be56688f409d4e2a3ea730
Onnda <dannoman2004@gmail.com>
2025-01-22 01:06:03 +0100

    InitialScene logic

---
+++ WaltersWalk/Assets/WaltersWalk/WW_Art/Modelos Dani/PortalInicio/OpenDoor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+	public Animator Puerta;
+ 
+	public void StartGame()
+	{
+		Puerta.SetBool("NewGame", true);
+	}
+}
